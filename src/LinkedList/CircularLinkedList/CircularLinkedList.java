8f1f3dcf0f808e4fb8da9c458f977ab41ed6d27b
deepanshu-aggarwal <deepanshu.26.da@gmail.com>
2021-12-04 23:12:17 +0530

    Circular Linked List

--- src/LinkedList/CircularLinkedList/CircularLinkedList.java
+++ src/LinkedList/CircularLinkedList/CircularLinkedList.java
@@ -10,7 +10,12 @@ public class CircularLinkedList {
         head.next.next.next.next=head;
 
         traverse(head);
-        head=insertAtBegin(head,1);
+//        head=insertAtBegin(head,1);
+//        head=insertAtEndEfficient(head,108);
+//        traverse(head);
+//        head=deleteAtBegin(head);
+//        head=deleteAtBeginEfficient(head);
+        head=deleteKthNode(head,5);
         traverse(head);
     }
 
@@ -46,6 +51,79 @@ public class CircularLinkedList {
         curr.next=temp;
         temp.next=head;
         return temp;
+    }
+
+    static Node insertAtEnd(Node head,int x)
+    {
+        Node temp=new Node(x);
+        if(head==null)
+        {
+            temp.next=temp;
+            return temp;
+        }
+        Node curr=head;
+        while(curr.next!=head)
+        {
+            curr=curr.next;
+        }
+        curr.next=temp;
+        temp.next=head;
+        return head;
+    }
+
+    static Node insertAtEndEfficient(Node head,int x)
+    {
+        Node temp=new Node(x);
+        if(head==null)
+        {
+            temp.next=temp;
+            return temp;
+        }
+        temp.next=head.next;
+        head.next=temp;
+        int data=temp.data;
+        temp.data=head.data;
+        head.data=data;
+        return temp;
+    }
+
+    static Node deleteAtBegin(Node head)
+    {
+        if(head==null || head.next==null)
+            return null;
+        Node curr=head;
+        while(curr.next!=head)
+            curr=curr.next;
+        curr.next=head.next;
+        return head.next;
+    }
+
+    static Node deleteAtBeginEfficient(Node head)
+    {
+        if(head==null || head.next==head)
+            return null;
+        head.data=head.next.data;
+        head.next=head.next.next;
+        return head;
+    }
+
+    static Node deleteKthNode(Node head,int k)
+    {
+        if(head==null)
+            return null;
+        if(k==1)
+            return deleteAtBeginEfficient(head);
+        Node curr=head;
+        int i=1;
+        while(i<k-1)
+        {
+            curr=curr.next;
+            i++;
+        }
+        if(curr.next==head)
+            head=curr.next.next;
+        curr.next=curr.next.next;
+        return head;
     }
 }
 
