504b51013c9e2ff903fb4fbb26e3c01aebce25af
deepanshu-aggarwal <deepanshu.26.da@gmail.com>
2022-01-30 21:34:50 +0530

    DP Problems

--- src/DynamicProgramming/Count_Subset_Sum.java
+++ src/DynamicProgramming/Count_Subset_Sum.java
@@ -6,10 +6,15 @@ public class Count_Subset_Sum {
         int n=arr.length;
         int target=9;
 
-        System.out.println(countSubset(arr, target, n));
+        System.out.println(recCountSubset(arr, target, n));
+
+        int[][] dp = new int[n+1][target+1];
+        System.out.println(dynCountSubset(arr, target, dp, n));
+
+        System.out.println(tabCountSubset(arr, target, n));
     }
 
-    public static int countSubset(int[] arr, int target, int n)
+    public static int recCountSubset(int[] arr, int target, int n)
     {
         if(n==0)
         {
@@ -20,9 +25,62 @@ public class Count_Subset_Sum {
 
         if(target >= arr[n-1])
         {
-            return countSubset(arr, target-arr[n-1], n-1) + countSubset(arr, target, n-1);
+            return recCountSubset(arr, target-arr[n-1], n-1) + recCountSubset(arr, target, n-1);
         }
         else
-            return countSubset(arr, target, n-1);
+            return recCountSubset(arr, target, n-1);
+    }
+
+    public static int dynCountSubset(int[] arr, int sum, int[][] dp, int n)
+    {
+        if(n==0)
+        {
+            if(sum==0)
+                return 1;
+            return 0;
+        }
+
+        if(dp[n][sum] != 0)
+            return dp[n][sum];
+
+        if(sum >= arr[n-1])
+        {
+            dp[n][sum] = dynCountSubset(arr, sum-arr[n-1], dp, n-1) + dynCountSubset(arr, sum, dp, n-1);
+            return dp[n][sum];
+        }
+        else
+        {
+            dp[n][sum] = dynCountSubset(arr, sum, dp, n-1);
+            return dp[n][sum];
+        }
+    }
+
+    public static int tabCountSubset(int[] arr, int sum, int n)
+    {
+        int[][] tab = new int[n+1][sum+1];
+
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0)
+                    tab[i][j] = 0;
+                if(j==0)
+                    tab[i][j] = 1;
+            }
+        }
+
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(j >= arr[i-1])
+                    tab[i][j] = tab[i-1][j-arr[i-1]] + tab[i-1][j];
+                else
+                    tab[i][j] = tab[i-1][j];
+            }
+        }
+
+        return tab[n][sum];
     }
 }
--- src/DynamicProgramming/Equal_Sum_Partition.java
+++ src/DynamicProgramming/Equal_Sum_Partition.java
@@ -7,8 +7,16 @@ public class Equal_Sum_Partition {
         int sum=0;
         for(int i: arr)
             sum+=i;
+
         if(sum%2==0)
+        {
             System.out.println(recEqualPart(arr, sum/2, n));
+
+            boolean[][] dp = new boolean[n+1][sum/2+1];
+            System.out.println(dynEqualPart(arr, sum/2, dp, n));
+
+            System.out.println(tabEqualPart(arr, sum/2, n));
+        }
         else
             System.out.println("false");
     }
@@ -24,5 +32,51 @@ public class Equal_Sum_Partition {
         }
         else
             return recEqualPart(arr, sum, n-1);
+    }
+
+    public static boolean dynEqualPart(int[] arr, int sum, boolean[][] dp, int n)
+    {
+        if(n==0)
+        {
+            return sum == 0;
+        }
+
+        if(dp[n][sum])
+            return true;
+
+        if(sum >= arr[n-1])
+            return dynEqualPart(arr, sum-arr[n-1], dp, n-1) || dynEqualPart(arr, sum, dp, n-1);
+        else
+            return dynEqualPart(arr, sum, dp, n-1);
+    }
+
+    public static boolean tabEqualPart(int[] arr, int sum, int n)
+    {
+        boolean[][] tab = new boolean[n+1][sum+1];
+
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0)
+                    tab[i][j] = false;
+
+                if(j==0)
+                    tab[i][j] = true;
+            }
+        }
+
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(j >= arr[i-1])
+                    tab[i][j] = tab[i-1][j-arr[i-1]] || tab[i-1][j];
+                else
+                    tab[i][j] = tab[i-1][j];
+            }
+        }
+
+        return tab[n][sum];
     }
 }
