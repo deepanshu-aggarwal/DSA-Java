11c2863948f56484917962c8a452438ded5843a5
deepanshu-aggarwal <deepanshu.26.da@gmail.com>
2022-05-13 11:10:53 +0530

    Trie Implementation

--- src/Trie/Concept/Implement.java
+++ src/Trie/Concept/Implement.java
@@ -3,6 +3,8 @@ package Trie.Concept;
 class Node{
     Node[] links = new Node[26];
     boolean flag = false;
+    int cw = 0;
+    int ew = 0;
 
     boolean containsKey(char ch){
         return (links[ch-'a'] != null);
@@ -19,6 +21,18 @@ class Node{
     void setEnd(){
         flag = true;
     }
+
+    void incrCW(){
+        cw++;
+    }
+
+    void incrEW(){
+        ew++;
+    }
+
+    void decrCW() { cw--; }
+
+    void decrEW() { ew--; }
 }
 
 class Trie{
@@ -36,8 +50,9 @@ class Trie{
                 node.put(ch, new Node());
             }
             node = node.get(ch);
+            node.incrCW();
         }
-        node.setEnd();
+        node.incrEW();
     }
 
     boolean search(String word){
@@ -61,12 +76,45 @@ class Trie{
         }
         return true;
     }
+
+    int countStartsWith(String word){
+        Node node = root;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(!node.containsKey(ch))   return 0;
+            node = node.get(ch);
+        }
+        return node.cw;
+    }
+
+    int countWordsEqualTo(String word){
+        Node node = root;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(node.containsKey(ch)){
+                node = node.get(ch);
+            }
+            else    return 0;
+        }
+        return node.ew;
+    }
+
+    void erase(String word){
+        Node node = root;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(!node.containsKey(ch))   return;
+            node = node.get(ch);
+            node.decrCW();
+        }
+        node.decrEW();
+    }
 }
 
 public class Implement {
     public static void main(String[] args) {
-        int type[] = {1, 1, 2, 3, 2};
-        String value[] = {"hello", "help", "help", "hel", "hel"};
+        int type[] = {1,1,1,6,1,5};
+        String value[] = {"hello", "help", "help", "help", "hel", "hel"};
         Trie trie = new Trie();
         for(int i=0; i<type.length; i++){
             if(type[i] == 1){
@@ -75,8 +123,17 @@ public class Implement {
             else if(type[i] == 2){
                 System.out.println(trie.search(value[i]));
             }
-            else{
+            else if(type[i] == 3){
                 System.out.println(trie.startsWith(value[i]));
+            }
+            else if(type[i] == 4){
+                System.out.println(trie.countWordsEqualTo(value[i]));
+            }
+            else if(type[i] == 5){
+                System.out.println(trie.countStartsWith(value[i]));
+            }
+            else{
+                trie.erase(value[i]);
             }
         }
     }
